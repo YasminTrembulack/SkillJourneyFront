d03ba3492aa115b4c10dabea13ea345e028b9ded
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-09-24 13:03:04 -0300

    feat: create Home page and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import "./styles/global.css"
 import LoginPage from "./Pages/LoginPage";
+import HomePage from "./Pages/HomePage";
+import RegisterPage from "./Pages/RegisterPage";
 
 export default function App() {
   return (
     <>
       <LoginPage/>
       <Routes>
-        <Route/>  
+        <Route path="/" element={<LoginPage/>}/>  
+        <Route path="/home" element={<HomePage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
       </Routes>     
     </>
   )
---
+++ src/Pages/HomePage/index.tsx
@@ -0,0 +1,10 @@
+// import Card from "./Components/Card";
+// import { StyledDiv, Image } from "./styles";
+
+export default function HomePage() {
+  return (
+    <>
+      
+    </>
+  );
+}
