c4bd881aed3c181b45a44892764b45a15fe7f4bf
jessifedalto <jessicafedalto.jf@gmail.com>
2024-10-02 10:34:24 -0300

    feat: home page prettier

---
+++ src/Components/CardHome/styles.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+  width: 16rem; /* 64 */
+  background-color: white;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.09);
+  padding: 2.25rem; /* 9 */
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem; /* space-y-3 */
+`;
+
+export const Circle = styled.div`
+  width: 6rem; /* 24 */
+  height: 6rem; /* 24 */
+  background-color: #7c3aed; /* bg-violet-500 */
+  border-radius: 50%;
+  position: absolute;
+  right: -1.25rem; /* -right-5 */
+  top: -1.75rem; /* -top-7 */
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+export const CircleText = styled.p`
+  color: white;
+  font-size: 1.5rem; /* text-2xl */
+  position: absolute;
+  bottom: 1.5rem; /* bottom-6 */
+  left: 1.75rem; /* left-7 */
+`;
+
+export const IconContainer = styled.div`
+  width: 3rem; /* w-12 */
+  max-width: 50px;
+  max-height: 100px;
+  fill: #7c3aed; /* fill-violet-500 */
+`;
+
+export const Title = styled.h1`
+  font-weight: bold;
+  font-size: 1.25rem; /* text-xl */
+`;
+
+export const Description = styled.p`
+  font-size: 0.875rem; /* text-sm */
+  color: #4b5563; /* text-zinc-500 */
+  line-height: 1.5; /* leading-6 */
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover
+`
--- src/Pages/HomePage/styles.ts
+++ src/Pages/HomePage/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const HomeContent = styled.div`
-  width: 75%;
+  width: 60%;
 `;
 
 export const MainContent = styled.div`
@@ -27,35 +27,71 @@ export const Welcome = styled.h3`
   justify-content: start;
   align-items: start;
   padding: 15px;
+  pointer-events: none;
   `
 
 export const ObjectiveContent = styled.div`
   width: 100%;
-  justify-content: center;
-  align-items: center;
   background-color: #FFFFFF;
   padding: 15px;
+  flex: 1;
+  text-align: center;
 `
 
 export const TitleObjective = styled.h4`
   display: flex;
-  justify-content: center;
-  align-items: center;
   font-weight: bold;
   font-size: larger;
+  color: #ECA400;
 `
 
 export const RecentTrainings = styled.div`
   width: 75%;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)); /* Ajuste o 200px conforme necessário */
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)); 
   align-items: center;
   gap: 20px;
   padding-bottom: 80px;
-  padding: 15px;
+
 `
 
 export const ObjectiveText = styled.p`
   font-size: large;
   font-weight: normal;
+  text-align: start;
+  padding-bottom: 10px;
+`
+
+export const TitleRecent = styled.p`
+  font-size: larger;
+  font-weight: bolder;
+  padding: 10px;
+`
+
+export const MainObjective = styled.p`
+    font-size: large;
+    font-weight: bold;
+    color: blueviolet;
+`
+
+export const ObjectiveContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+  background-color: #FFFFFF;
+  border: solid white;
+  border-radius: 5px;
+`
+
+export const ObjectiveTextContainer = styled.div`
+  display: flex; 
+  flex-direction: column; 
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`
+
+export const TitleDiv = styled.div`
+  display: flex;
+  justify-content: center;
 `
