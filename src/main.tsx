3e7838d08f38eb0e8291355e8fe7fa9dfb090ae4
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-10-29 10:14:55 -0300

    feat: add speed-insighta

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./Styles/global.css"
 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <SpeedInsights/>
     <BrowserRouter>
       <App />
     </BrowserRouter>
