937dfb375b619fb863953cd74c2e790169c203af
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-10-29 10:47:53 -0300

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react"
 import "./Styles/global.css"
 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <SpeedInsights/>
+    <Analytics/>
     <BrowserRouter>
       <App />
     </BrowserRouter>
