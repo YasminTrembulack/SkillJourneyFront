be1c4f20713e1c6de4aad3c6b72d3e6c9c7e1961
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-10-30 10:13:12 -0300

    feat: changing api url

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "http://localhost:8080/sjapi/",
+    baseURL: "https://skilljourneyback.onrender.com/sjapi/",
     timeout: 8000
 })
